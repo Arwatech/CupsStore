d3067ede89af5435300ca71dc1bec5055deb09ae
Arwatech <rookaturki@hotmail.com>
2022-07-29 09:27:46 +0300

    Initial Commit

---
+++ Store/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Store
+//
+//  Created by Arwa Alzahrani on 30/12/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Store/StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreApp.swift
+//  Store
+//
+//  Created by Arwa Alzahrani on 30/12/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
